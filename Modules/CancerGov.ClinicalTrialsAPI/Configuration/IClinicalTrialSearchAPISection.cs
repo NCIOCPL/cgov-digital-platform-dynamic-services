ebad141c72d773cec53b046444b87d9f8f7287d7
Blair Learn <blairl-nih@users.noreply.github.com>
2022-03-24 14:12:30 -0400

    (#23) Add the v2 Clinical Trials client library.

---
+++ Modules/CancerGov.ClinicalTrialsAPI/Configuration/IClinicalTrialSearchAPISection.cs
@@ -0,0 +1,15 @@
+﻿namespace CancerGov.ClinicalTrialsAPI
+{
+    public interface IClinicalTrialSearchAPISection
+    {
+        /// <summary>
+        /// Retrieves the API key.
+        /// </summary>
+        string APIKey { get; }
+
+        /// <summary>
+        /// Gets the base URL of the Clinical Trials API.
+        /// </summary>
+        string BaseUrl { get; }
+    }
+}
